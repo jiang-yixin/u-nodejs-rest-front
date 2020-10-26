d9b30b14c3a198895b92e173bf7d715e4f978795
Yixin Jiang <jiangyixin0910@gmail.com>
2020-10-26 22:13:14 +0100

    Adding auth in headers and update some API urls

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -22,7 +22,12 @@ class Feed extends Component {
   };
 
   componentDidMount() {
-    fetch('URL')
+    fetch('http://localhost:8080/user/status', {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer ' + this.props.token
+      }
+    })
       .then(res => {
         if (res.status !== 200) {
           throw new Error('Failed to fetch user status.');
@@ -50,7 +55,11 @@ class Feed extends Component {
       page--;
       this.setState({ postPage: page });
     }
-    fetch('http://localhost:8080/feed/posts')
+    fetch('http://localhost:8080/feed/posts?page=' + page, {
+      headers: {
+        'Authorization': 'Bearer ' + this.props.token
+      }
+    })
       .then(res => {
         if (res.status !== 200) {
           throw new Error('Failed to fetch posts.');
@@ -74,7 +83,14 @@ class Feed extends Component {
 
   statusUpdateHandler = event => {
     event.preventDefault();
-    fetch('URL')
+    fetch('http://localhost:8080/user/status', {
+      method: 'PUT',
+      headers: {
+        'Authorization': 'Bearer ' + this.props.token,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ status: this.state.status })
+    })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error("Can't update status!");
@@ -125,6 +141,9 @@ class Feed extends Component {
     fetch(url, {
       method,
       body: formData,
+      headers: {
+        'Authorization': 'Bearer ' + this.props.token
+      }
     })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
@@ -177,6 +196,9 @@ class Feed extends Component {
     this.setState({ postsLoading: true });
     fetch('http://localhost:8080/feed/post/' + postId, {
       method: 'DELETE',
+      headers: {
+        'Authorization': 'Bearer ' + this.props.token
+      }
     })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
